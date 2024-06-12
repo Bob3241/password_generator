a45e2dcebbf23cce49d1397408663855e16614c6
Bob3241 <slava.sundukov.2014@mail.ru>
2024-06-12 19:03:33 +0300

    intial commit

---
+++ script_password_chage.js
@@ -0,0 +1,13 @@
+let n = 1;
+
+function doFunction()
+{
+    var str = n++;
+    let element = document.getElementById("password");
+    element.innerHTML = str;
+    element.style
+}
+
+const el = document.getElementById("button");
+console.log(el)
+el.addEventListener("click", doFunction);
